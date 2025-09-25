4a0fe5c426d931f627c98242e41016a3ab58ccb8
Etec Prof. Horácio Augusto da Silveira <limajeffersonn654@gmail.com>
2025-09-25 15:12:51 -0300

    manifest

--- MajorBeat/MajorBeat/Views/Musico/MusicianProfileView.xaml.cs
+++ MajorBeat/MajorBeat/Views/Musico/MusicianProfileView.xaml.cs
@@ -23,4 +23,33 @@ public partial class MusicianProfileView : ContentPage
     {
         await Navigation.PushAsync(new UserRegisterView());
     }
+
+    private async void OnAddPhotoClicked(object sender, EventArgs e)
+    {
+        string action = await DisplayActionSheet("Adicionar Foto", "Cancelar", null, "Escolher da Galeria", "Tirar Foto");
+
+        FileResult photo = null;
+
+        try
+        {
+            if (action == "Escolher da Galeria")
+            {
+                photo = await MediaPicker.PickPhotoAsync();
+            }
+            else if (action == "Tirar Foto")
+            {
+                photo = await MediaPicker.CapturePhotoAsync();
+            }
+
+            if (photo != null)
+            {
+                var stream = await photo.OpenReadAsync();
+                SelectedImage.Source = ImageSource.FromStream(() => stream);
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Erro", "Não foi possível obter a imagem: " + ex.Message, "OK");
+        }
+    }
 }
